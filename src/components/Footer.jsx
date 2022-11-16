c1114af98da8202dc5cb47e4751434cbc938a9cc
Dan Henry <dhenry437@gmail.com>
2022-11-16 20:04:40 +1100

    base html for nav, main, and footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="footer mt-auto py-3">
+      <div className="container">
+        <span>
+          Made with &#10084; in Melbourne.
+          <br />
+          &copy; 2022 Broccolli & Co. All rights reserved.
+        </span>
+      </div>
+    </footer>
+  );
+}
---
+++ src/components/Main.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Main() {
+  return (
+    <main className="d-flex flex-shrink-0 flex-grow-1 align-items-center">
+      <div className="container p-2">
+        <h1 className="m-3">
+          A better way <br />
+          to enjoy every day.
+        </h1>
+        <p className="mb-4">Be the first to know when we launch.</p>
+        <button>Request an invite</button>
+      </div>
+    </main>
+  );
+}
